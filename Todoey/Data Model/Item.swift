7b7105b7d312b63aa986032d4b69a49b5d22c4ec
Heikki Mattila <heikki.mattila0@hotmail.com>
2019-08-05 19:34:10 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Heikki Mattila on 03/08/2019.
+//  Copyright © 2019 MonkeyPod. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
